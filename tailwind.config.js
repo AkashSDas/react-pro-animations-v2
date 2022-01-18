86dfe6147dbfd73f2b1c07916836653e25879c9c
AkashSDas <aakashdas368@gmail.com>
2022-01-18 11:37:09 +0530

    add card color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       transparent: "transparent",
       primary: "#1C1A1C",
       secondary: "#FFFEFF",
+      card: "#27282E",
       text: "#CFCED0",
       lightPink: "#DAB8C5",
       lightPurple: "#B694F1",
